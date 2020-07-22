17eeae79494d27a76d0e4690ec9359ee7eafb34f
Glitch (codersx-express-lesson1) <none>
2020-07-22 19:03:33 +0000

    ☎️🎏 Checkpoint ./server.js:19548641/365

--- server.js
+++ server.js
@@ -7,14 +7,11 @@ app.get("/", (req, res) => {
   res.send("I love CodersX");
 });
 var todos = ["Đi chợ", "Nấu Cơm", "Rửa Bát", "Học code tại CoderX"];
+var port = 3000;
 app.get("/todos", (req, res) => {
   res.render("todos/index", {
     todos: todos
 });
-// listen for requests :)
 app.listen(process.env.PORT, () => {
   console.log("Server listening on port " + process.env.PORT);
 });
-
-var q = req.query.q;
-var matchNau = todos.filter(function())
